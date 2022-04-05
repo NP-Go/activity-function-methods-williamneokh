7e509d535c07dc91430da378e93c3ff68616686b
williamneo <williamneokh@gmail.com>
2022-04-05 13:44:10 +0800

    done

--- customer.go
+++ customer.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
 //Declare a struct
+type customer struct {
+	fName    string
+	lName    string
+	userName string
+	password string
+	email    string
+	phone    int
+	address  string
+}
+
+func (c customer) userCredentials() (string, string) {
+	return c.userName, c.password
+}
+
+func (c customer) userAddress() string {
+	return c.address
+}
+func (c customer) allUserInformation() {
+	fmt.Printf("First Name: %v\n"+
+		"Last Name: %v\n"+
+		"Username: %v\n"+
+		"Password: %v\n"+
+		"Email: %v\n"+
+		"Phone: %v\n"+
+		"Address: %v\n\n", c.fName, c.lName, c.userName, c.password, c.email, c.phone, c.address)
+}
 
 //Create Methods
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Declare new item customer
+	customer1 := customer{}
+	customer1 = customer{
+		fName:    "Micheal",
+		lName:    "Jordan",
+		userName: "MJ2020",
+		password: "1234567",
+		email:    "MJ2020@gmail.com",
+		phone:    12345678,
+		address:  "18227 Capstan Greens Road Cornelius, NC 28031",
+	}
+
+	customer1.allUserInformation()
+
+	cUsername, cPassword := customer1.userCredentials()
+	fmt.Printf("Username: %v\nPassword: %v\n", cUsername, cPassword)
+
+	cUserAddress := customer1.userAddress()
+	fmt.Printf("Customer Address: %v\n", cUserAddress)
 
 	//Insert Code here
 }
